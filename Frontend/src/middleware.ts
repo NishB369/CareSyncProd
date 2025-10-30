9cbde13f02102ef65dbe5944d2d831518bc26f87
nishb <nishbcodes@gmail.com>
2025-10-30 14:33:46 +0530

    Bug Fix Testing

--- Frontend/src/middleware.ts
+++ Frontend/src/middleware.ts
@@ -1,55 +1,26 @@
-// import { NextRequest, NextResponse } from "next/server";
-
-// export function middleware(request: NextRequest) {
-//   const careSyncAccessToken = request.cookies.get("careSyncAccessToken")?.value;
-//   const isLoggedIn = !!careSyncAccessToken;
-//   const currentPath = request.nextUrl.pathname;
-
-//   const publicRoutes = [
-//     "/",
-//     "/landingpage",
-//     "/auth/login/manager",
-//     "/alertpage",
-//     "/documentation",
-//   ];
-
-//   if (!isLoggedIn) {
-//     if (publicRoutes.includes(currentPath)) {
-//       return NextResponse.next();
-//     }
-//     return NextResponse.redirect(new URL("/landingpage", request.url));
-//   }
-
-//   // If logged in
-//   if (isLoggedIn) {
-//     if (currentPath === "/auth/login/manager") {
-//       return NextResponse.redirect(new URL("/manager/home", request.url));
-//     }
-//     return NextResponse.next();
-//   }
-// }
-
-// export const config = {
-//   matcher: [
-//     "/((?!_next/static|_next/image|favicon.ico|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.svg|.*\\.gif|_next/data).*)",
-//   ],
-// };
-
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const careSyncAccessToken = request.cookies.get("careSyncAccessToken")?.value;
   const isLoggedIn = !!careSyncAccessToken;
-
   const currentPath = request.nextUrl.pathname;
 
+  const publicRoutes = [
+    "/",
+    "/landingpage",
+    "/auth/login/manager",
+    "/alertpage",
+    "/documentation",
+  ];
+
   if (!isLoggedIn) {
-    if (currentPath === "/auth/login/manager") {
+    if (publicRoutes.includes(currentPath)) {
       return NextResponse.next();
     }
-    return NextResponse.redirect(new URL("/auth/login/manager", request.url));
+    return NextResponse.redirect(new URL("/landingpage", request.url));
   }
 
+  // If logged in
   if (isLoggedIn) {
     if (currentPath === "/auth/login/manager") {
       return NextResponse.redirect(new URL("/manager/home", request.url));
