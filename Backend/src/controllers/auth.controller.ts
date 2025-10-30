31872f403b16d1c928a86fc2b9b2753082b01e35
nishb <nishbcodes@gmail.com>
2025-10-30 13:44:33 +0530

    Bug Fix Testing

--- Backend/src/controllers/auth.controller.ts
+++ Backend/src/controllers/auth.controller.ts
@@ -9,7 +9,7 @@ export const login = async (req: Request, res: Response) => {
   res.cookie("careSyncAccessToken", accessToken, {
     httpOnly: true,
     secure: true,
-    sameSite: "none" as const,
+    sameSite: "lax" as const,
     path: "/",
     maxAge: 1000 * 60 * 60 * 24,
   });
